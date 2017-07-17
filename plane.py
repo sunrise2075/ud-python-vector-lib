0e278bfbb021f6ff3bbd36013471ea9eaa7e3215
sunrise2075 <sunrise2075@163.com>
2017-07-17 08:12:04 +0800

    coding testing

--- plane.py
+++ plane.py
@@ -139,24 +139,24 @@ class MyDecimal(Decimal):
 
 
 
-print "#  1."
-p1 = Plane(normal_vector= Vector(['-0.412', '3.806', '0.728']), constant_term='-3.46')
-p2 = Plane(normal_vector= Vector(['1.03', '-9.515', '-1.82']), constant_term= '8.65')
-print 'first pair of planes are parallel?:{}' .format(p1.is_parallel_to(p2))
-print 'first pair of planes are equal?:{}' .format(p1 == p2)
+# print "#  1."
+# p1 = Plane(normal_vector= Vector(['-0.412', '3.806', '0.728']), constant_term='-3.46')
+# p2 = Plane(normal_vector= Vector(['1.03', '-9.515', '-1.82']), constant_term= '8.65')
+# print 'first pair of planes are parallel?:{}' .format(p1.is_parallel_to(p2))
+# print 'first pair of planes are equal?:{}' .format(p1 == p2)
 
-print "#  2."
-p1 = Plane(normal_vector= Vector(['2.611', '5.528', '0.283']), constant_term='4.6')
-p2 = Plane(normal_vector= Vector(['7.715', '8.306', '5.342']), constant_term= '3.76')
-print 'second pair of planes are parallel?:{}' .format(p1.is_parallel_to(p2))
-print 'second pair of planes are equal?:{}' .format(p1 == p2)
+# print "#  2."
+# p1 = Plane(normal_vector= Vector(['2.611', '5.528', '0.283']), constant_term='4.6')
+# p2 = Plane(normal_vector= Vector(['7.715', '8.306', '5.342']), constant_term= '3.76')
+# print 'second pair of planes are parallel?:{}' .format(p1.is_parallel_to(p2))
+# print 'second pair of planes are equal?:{}' .format(p1 == p2)
 
-print "#  3."
-p3 = Plane(normal_vector= Vector(['-7.926', '8.625', '-7.212']), constant_term='-7.952')
-p4 = Plane(normal_vector= Vector(['-2.642', '2.875', '-2.404']), constant_term= '-2.443')
+# print "#  3."
+# p3 = Plane(normal_vector= Vector(['-7.926', '8.625', '-7.212']), constant_term='-7.952')
+# p4 = Plane(normal_vector= Vector(['-2.642', '2.875', '-2.404']), constant_term= '-2.443')
 
-print 'third pair of planes are parallel?:{}' .format(p3.is_parallel_to(p4))
-print 'third pair of planes are equal?:{}' .format(p3 == p4)
+# print 'third pair of planes are parallel?:{}' .format(p3.is_parallel_to(p4))
+# print 'third pair of planes are equal?:{}' .format(p3 == p4)
 
 
 
